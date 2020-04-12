c5bb65510d5a496939d537c75283756eb027c827
刘占彬 <liuzhanbin@liuzhanbindeMacBook-Pro.local>
2020-04-12 22:08:51 +0800

    day06

--- studygo/day06/mylogger/console.go
+++ studygo/day06/mylogger/console.go
@@ -7,56 +7,56 @@ import (
 
 // 往终端写入日志相关内容
 
-// Logger 日志结构体
-type Logger struct {
+// ConsloeLogger 日志结构体
+type ConsloeLogger struct {
 	Level LogLevel
 }
 
-// NewLog 构造函数
-func NewLog(levelStr string) Logger {
+// NewConsoleLogger 构造函数
+func NewConsoleLogger(levelStr string) ConsloeLogger {
 	level, err := parseLogLevel(levelStr)
 	if err != nil {
 		panic(err)
 	}
-	return Logger{
+	return ConsloeLogger{
 		Level: level,
 	}
 }
 
+func (l ConsloeLogger) enable(LogLevel LogLevel) bool {
+	return LogLevel >= l.Level
+}
+
+// Ptime ...
 func Ptime() string {
 	now := time.Now()
 	return now.Format("2006-01-02 15:04:05")
 }
 
-func (l Logger) enable(LogLevel LogLevel) bool {
-	return LogLevel >= l.Level
-}
-
-func log(lv LogLevel, msg string) {
-	funcName, FileName, lineNo := getInfo(3)
-	fmt.Printf("[%s][%s][%s:%s:%d] %s\n", Ptime(), getLogString(lv), FileName, funcName, lineNo, msg)
-}
-
-func (l Logger) Debug(msg string) {
-	if l.enable(DEBUG) {
-		log(DEBUG, msg)
+func (l ConsloeLogger) log(lv LogLevel, format string, a ...interface{}) {
+	if l.enable(lv) {
+		msg := fmt.Sprintf(format, a...)
+		funcName, FileName, lineNo := getInfo(3)
+		fmt.Printf("[%s][%s][%s:%s:%d] %s\n", Ptime(), getLogString(lv), FileName, funcName, lineNo, msg)
 	}
 }
 
-func (l Logger) Info(msg string) {
-	if l.enable(INFO) {
-		log(INFO, msg)
-	}
+// Debug ...
+func (l ConsloeLogger) Debug(format string, a ...interface{}) {
+	l.log(DEBUG, format, a...)
 }
 
-func (l Logger) Warning(msg string) {
-	if l.enable(WARNING) {
-		log(WARNING, msg)
-	}
+// Info ...
+func (l ConsloeLogger) Info(format string, a ...interface{}) {
+	l.log(INFO, format, a...)
 }
 
-func (l Logger) Error(msg string) {
-	if l.enable(ERROR) {
-		log(ERROR, msg)
-	}
+// Warning ...
+func (l ConsloeLogger) Warning(format string, a ...interface{}) {
+	l.log(WARNING, format, a...)
+}
+
+// Debug ...
+func (l ConsloeLogger) Error(format string, a ...interface{}) {
+	l.log(ERROR, format, a...)
 }
